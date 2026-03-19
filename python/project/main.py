d132f8117d21896d36ff127481c47c7c5b881ce2
Student <Student@Train11-4-PC056.control.ad.sit.kmutt.ac.th>
2026-03-19 16:13:48 +0700

    Update main.py

--- python/project/main.py
+++ python/project/main.py
@@ -1,6 +1,13 @@
 import numpy as np
 import pandas as pd
 
-df = pd.read_csv('asthma_disease_data_new.csv')
+df = pd.read_csv('asthma_disease_data_new.csv')    
+pd.set_option('display.max_rows',None)
 
-print(df)
+def cleaning():
+    #clean PID (fill up the missing value)
+    PID = df["PatientID"]
+    cleaned_PID = PID.interpolate(method= "linear").astype(int)
+    print(cleaned_PID)
+
+cleaning()
